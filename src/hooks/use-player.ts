f33bbf95c1c5ba0c3b7ff3463ced9a2c4a7a83b4
Yaroslav <coorum.y@gmail.com>
2025-02-20 16:18:51 +0100

    Update usePlayer hook to check for ref presence

--- src/hooks/use-player.ts
+++ src/hooks/use-player.ts
@@ -1,33 +1,45 @@
-import { RefObject, useEffect, useState } from "react";
-
+import { useEffect, useState, type RefObject } from "react";
 import type { PlayerRef, CallbackListener } from "@remotion/player";
 
 export const usePlayer = (ref: RefObject<PlayerRef | null>) => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    if (!ref.current) return;
+    const checkRefAvailability = () => {
+      if (ref.current) {
+        const onPlay: CallbackListener<"play"> = () => {
+          setIsPlaying(true);
+          ref.current?.requestFullscreen();
+        };
 
-    const onPlay: CallbackListener<"play"> = () => {
-      setIsPlaying(true);
-      ref.current?.requestFullscreen();
+        const onPause: CallbackListener<"pause"> = () => {
+          setIsPlaying(false);
+          ref.current?.exitFullscreen();
+        };
+
+        ref.current.addEventListener("play", onPlay);
+        ref.current.addEventListener("pause", onPause);
+
+        // Cleanup function to remove event listeners
+        return () => {
+          ref.current?.removeEventListener("play", onPlay);
+          ref.current?.removeEventListener("pause", onPause);
+        };
+      }
+
+      return null;
     };
 
-    const onPause: CallbackListener<"pause"> = () => {
-      setIsPlaying(false);
-      ref.current?.exitFullscreen();
-    };
+    const intervalId = setInterval(() => {
+      const cleanup = checkRefAvailability();
 
-    ref.current.addEventListener("play", onPlay);
-    ref.current.addEventListener("pause", onPause);
+      if (cleanup) {
+        clearInterval(intervalId);
+      }
+    }, 500);
 
-    return () => {
-      if (!ref.current) return;
-
-      ref.current.removeEventListener("play", onPlay);
-      ref.current.removeEventListener("pause", onPause);
-    };
-  }, []);
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
+  }, [ref]);
 
   return { isPlaying };
 };
